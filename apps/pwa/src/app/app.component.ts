bc7f550461aec557eac03431fec91474e58114f9
miaborde <38043788+mIaborde@users.noreply.github.com>
2023-08-25 22:19:23 +0200

    build: clean up

--- apps/pwa/src/app/app.component.ts
+++ apps/pwa/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule],
   selector: 'pwa-starter-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styles: [],
 })
 export class AppComponent {
   title = 'pwa';
