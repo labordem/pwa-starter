068385e539c1ae20387e98142d20ee1f654c425b
miaborde <38043788+mIaborde@users.noreply.github.com>
2023-08-25 22:19:23 +0200

    build: npm i -D @angular/pwa && nx g @angular/pwa:ng-add

--- apps/pwa/src/app/app.config.ts
+++ apps/pwa/src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation()), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
 };
